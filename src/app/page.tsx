5c5faf637264dd9ad0b8ffb09e8c8d523fab9c55
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2026-04-18 15:07:23 +1200

    starfield bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import Starfield from "./starfield";
+
 export default function Home() {
   return (
     <div>
-      <div className="w-1/2 h-16 bg-blue-200 flex items-center justify-center mb-4 text-black mx-auto rounded-lg">
-        Pick Up Planet
-      </div>
-      <div className="w-1/2 h-16 bg-green-200 flex items-center justify-center mb-4 text-black mx-auto rounded-lg">
-        Drop Off Planet
-      </div>
+		<Starfield />
+		<div className="w-1/4 h-16 bg-blue-200 flex items-center justify-center mb-4 text-black mx-auto rounded-lg">
+			Pick Up Planet
+		</div>
+		<div className="w-1/4 h-16 bg-green-200 flex items-center justify-center mb-4 text-black mx-auto rounded-lg">
+			Drop Off Planet
+		</div>
+		<button className="w-1/4 h-16 flex items-center justify-center mb-4 mx-auto">
+			Submit
+		</button>
     </div>
   );
 }
