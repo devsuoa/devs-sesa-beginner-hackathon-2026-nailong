c9cab0ca97d9ed2d8d2dff3b25527b8dc5579fc5
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2026-04-18 15:41:55 +1200

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,18 +17,28 @@ export default function Home() {
             alt="Landing Planet"
             width={400}
             height={400}
-            className="object-contain drop-shadow-[0_0_40px_rgba(255,255,255,0.4)]"
+            className="object-contain drop-shadow-[0_0_40px_rgba(255,255,255,0.4)] animate-[float_6s_ease-in-out_infinite]"
           />
         </div>
 
         {/* RIGHT: Inputs + Button */}
         <div className="w-1/2 flex flex-col items-center">
+
+			<h1
+				className="mb-6 text-center text-4xl md:text-5xl font-black tracking-[0.15em] text-white animate-[float_6s_ease-in-out_infinite] hover:tracking-[0.25em] transition-all duration-500"
+				style={{
+					textShadow:
+					"0 0 20px rgba(255,255,255,0.8), 0 0 40px rgba(255,255,255,0.4)",
+				}}
+			>
+				NAILONG EXPRESS
+			</h1>
           
-          <div className="w-full h-16 bg-blue-200 flex items-center justify-center mb-4 text-black rounded-lg">
+          <div className="flex items-center justify-center mb-4">
             Pick Up Planet
           </div>
 
-          <div className="w-full h-16 bg-green-200 flex items-center justify-center mb-4 text-black rounded-lg">
+          <div className="flex items-center justify-center mb-4">
             Drop Off Planet
           </div>
 
