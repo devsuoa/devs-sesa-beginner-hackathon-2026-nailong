6624975e36263f3ac5c41f986844a3886701ed78
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2026-04-18 13:48:42 +1200

    start background

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
+import Starfield from "./starfield";
+
 export default function Home() {
   return (
     <div>
+      <Starfield />
       <div className="w-full h-16 bg-yellow-200 flex items-center justify-center mb-4 text-black">
         NaiLong Express 
       </div>
       <div className="w-1/2 h-16 bg-blue-200 flex items-center justify-center mb-4 text-black mx-auto rounded-lg">
-        Login as Driver 
+        Pick Up Planet
       </div>
       <div className="w-1/2 h-16 bg-green-200 flex items-center justify-center mb-4 text-black mx-auto rounded-lg">
-        Login as Rider
+        Drop Off Planet
       </div>
     </div>
   );
